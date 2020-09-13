11b17aa020730eec9763947e8a82db81f267b921
Achint <achintdawra740@gmail.com>
2020-09-13 18:15:30 +0530

    Create_Exam

---
+++ public/Teachers/create_script.js
@@ -0,0 +1,28 @@
+$('#create_exam').on('click',()=>{
+    //Send a request to backend to create a database 
+    //for this subject
+
+})
+
+$('#create_new').on('click',()=>{
+
+    console.log('New Question Button Clicked');
+
+    
+    $('#ques_list').append("<br><input type='text' value='Enter Ques Number'>")
+    $('#ques_list').append("    <input type='text' class='ques' value='Enter Ques here'>");
+    $("#ques_list").append("    <button id='add_ques' onclick=adder()>ADD/UPDATE</button>");
+    $("#ques_list").append("    <button id='del_ques' onclick=deleter()>DELETE</button><br><br>")
+        
+});   
+
+        
+function adder(){
+    console.log("Add/Update Question Button clicked");
+    
+}
+
+function deleter(){
+    console.log("Delete Question Button clicked");
+
+}
