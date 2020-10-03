a6de9f13880325b48be5ffef35579da666962b85
Achint <achintdawra740@gmail.com>
2020-10-03 19:40:05 +0530

    BackEnd_Storing_Responses

--- Routes/exam.js
+++ Routes/exam.js
@@ -39,18 +39,14 @@ route.post('/view',function(req,res){
     });
 });
 
-
-//-----------------------------POST REQUEST FOR FINALISE EXAM -----------------------------
-route.post('/finalise',function(req,res){
-
-    let col = [];
-
-    questions.findAll({
-        where : {
-            sub_code : req.body.sub_code
-        }
-    }).then((ques_list)=>{
-        console.log(ques_list.length);
+function build_col(ques_list){
+    return new Promise(function(resolve,reject){
+        let col = [];
+        col.push({
+            header : "Username",
+            key : "username",
+            width : 25
+        });
         ques_list.forEach((q)=>{
             let id = (q.dataValues).id;
             let obj = {
@@ -60,9 +56,12 @@ route.post('/finalise',function(req,res){
             }
             col.push(obj);
         })
+        resolve(col);
     })
+}
 
-    var newsubject_sheet = workbook.addWorksheet(req.body.sub_code);
+function buildSheet(sub_code,col){
+    var newsubject_sheet = workbook.addWorksheet(sub_code);
     newsubject_sheet.columns = col;
 
     var filename = "Responses.xlsx";
@@ -72,7 +71,25 @@ route.post('/finalise',function(req,res){
     }).catch((err)=>{
         console.log(err);
     });
+}
 
+
+//-----------------------------POST REQUEST FOR FINALISE EXAM -----------------------------
+route.post('/finalise',function(req,res){
+
+    questions.findAll({
+        where : {
+            sub_code : req.body.sub_code
+        }
+    }).then((ques_list)=>{
+        console.log(ques_list.length);
+
+        build_col(ques_list)
+        .then((col)=>{
+            buildSheet(req.body.sub_code,col)
+            res.send('Success');
+        });
+    });
 
 });
 
@@ -80,35 +97,52 @@ route.post('/finalise',function(req,res){
 //-----------------------------POST REQUEST FOR SUBMIT EXAM -----------------------------
 route.post('/submit',function(req,res){
 
-    let m = calculate_and_update_marks(req.body,req.user);
+    let m = calculate_marks_store_responses_update_marks(req.body,req.user);
     //console.log("Marks Obtained = " + m);
 
     res.redirect('/student');
 
 });
 
-function calculate_and_update_marks(obj,student){
+function calculate_marks_store_responses_update_marks(obj,student){
     // for(key in obj){
     //     console.log(key + " --> " + obj[key]);
     // } 
     let total_marks = 0;
+    let user = student.username;
+    let code = obj.sub_code;
 
     questions.findAll({
         where : {
-            sub_code : obj.sub_code
+            sub_code : code
         }
     }).then((data)=>{
-        
+
+        let new_row = {};
+        new_row["username"] = user;
+
         data.forEach((q) => {
             console.log(q["id"] + "//" + q["answer"] + "//" + obj[q["id"]]);
+            new_row[q["id"]] = obj[q["id"]];
             if(q["answer"] === obj[q["id"]]){
                 total_marks+=1;
             }
         });
+
+        //Add Responses to the Excel Sheet
+        const worksheet = workbook.getWorksheet(code);
+        worksheet.addRow(new_row);
+        var filename = "Responses.xlsx";
+        workbook.xlsx.writeFile(filename).then(()=>{
+            // callback(null);
+            console.log('Sheet Updated Successfully');
+        }).catch((err)=>{
+            console.log(err);
+        });
+
+        //Add Marks to Database
         total_marks*=4;
         console.log("Returning marks are ",total_marks)
-        let user = student.username;
-        let code = obj.sub_code;
 
         marks.create({
             sub_code : code,
