72035f8c8d300795c4c9146e6cbc40637efc72ed
Achint <achintdawra740@gmail.com>
2020-10-09 00:23:32 +0530

    Teacher_Portal

---
+++ public/Teachers/script.js
@@ -0,0 +1,30 @@
+$(()=>{
+    $("#logout").hide();
+    $.get('/users/profile',(data)=>{
+        if(data.username==undefined){
+            alert("Please Login");
+            document.location.href='/login';
+        }
+        else if(data.category!="teacher"){
+            alert("Not Authorized");
+            document.location.href = '/';
+        }
+        else{
+            console.log("Welcome " + data.username);
+            $('#login123')
+                .text(data.username)
+                .attr("href","#")
+            $("#logout").show();
+        }
+    });
+});
+
+$('#logout').on('click',()=>{
+    $.get('/users/logout',(data)=>{
+        if(data=='Success'){
+        //Logged Out Successfully
+            alert("Logged Out! Please login to access the portal");
+            document.location.href = '/login';
+        }
+    })
+});
